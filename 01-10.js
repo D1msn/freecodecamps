9aa285c0ccaaab1f035208d2903d2b6f1727f8c9
D1msn <Flash.volkov@gmail.com>
2021-10-02 12:45:07 +0300

    10 complete

--- 01-10.js
+++ 01-10.js
@@ -22,3 +22,37 @@ a = 7;
 var b;
 b = a;
 
+//==== 5
+
+var a = 9;
+
+//==== 6
+
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+//==== 7
+
+// Variable declarations
+var studlyCapVar;
+var properCamelCase ;
+var titleCaseOver ;
+
+// Variable assignments
+studlyCapVar  = 10;
+properCamelCase  = "A String";
+titleCaseOver  = 9000;
+
+//==== 8
+
+var sum = 10 + 10;
+
+//==== 9
+
+var difference = 45 - 33;
+
+//==== 9
+
+var product = 8 * 10;
+
