7338ac645afd588eb91e375f23bb98828f7bc6b7
D1msn <Flash.volkov@gmail.com>
2021-10-03 18:00:05 +0300

    end 38 task

--- 28-38.js
+++ 28-38.js
@@ -2,3 +2,75 @@
 
 var myName = "Dmitry";
 var myStr = "My name is" + myName + "and I am well!";
+
+//==== 29
+
+var someAdjective = "yep";
+var myStr = "Learning to code is ";
+myStr += someAdjective;
+
+//==== 30
+
+// Setup
+var lastNameLength = 0;
+var lastName = "Lovelace";
+
+// Only change code below this line
+
+lastNameLength = lastName.length;
+
+//==== 31
+
+// Setup
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
+
+//==== 32
+
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+myStr = "Hello World"; // Change this line
+// Only change code above this line
+
+//==== 33
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var thirdLetterOfLastName = lastName[2]; // Change this line
+
+//==== 34
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var lastLetterOfLastName = lastName[lastName.length -1]; // Change this line
+
+//==== 35
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
+
+//==== 36
+
+// Only change code below this line
+var myArray = ["string", 22];
+
+//==== 37
+
+// Only change code below this line
+var myArray = [[1,2,3], [4,5,6]];
+
+//==== 38
+var myArray = [50,60,70];
+var myData = myArray[0];
