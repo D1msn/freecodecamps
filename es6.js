9dca89bf6026accf8ec7eafbe6212e76a7626cdb
D1msn <Flash.volkov@gmail.com>
2021-10-05 19:41:13 +0300

    50% complete

--- es6.js
+++ es6.js
@@ -74,3 +74,69 @@ let arr2;
 arr2 = [...arr1];  // Change this line
 
 console.log(arr2);
+
+// ===== 11
+const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+};
+
+// Only change code below this line
+const {today, tomorrow} = HIGH_TEMPERATURES;
+
+// Only change code above this line
+
+// ===== 12
+const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+};
+
+// Only change code below this line
+
+const {today: highToday, tomorrow:highTomorrow} = HIGH_TEMPERATURES;
+
+// Only change code above this line
+
+// ===== 13
+const LOCAL_FORECAST = {
+    yesterday: { low: 61, high: 75 },
+    today: { low: 64, high: 77 },
+    tomorrow: { low: 68, high: 80 }
+};
+
+// Only change code below this line
+const {today: {low: lowToday, high: highToday} } = LOCAL_FORECAST;
+
+// Only change code above this line
+
+// ===== 14
+let a = 8, b = 6;
+[a, b] = [b, a];
+// Only change code below this line
+
+// ===== 15
+const source = [1,2,3,4,5,6,7,8,9,10];
+function removeFirstTwo(list) {
+    // Only change code below this line
+    const [,,...arr] = list; // Change this line
+    // Only change code above this line
+    return arr;
+}
+const arr = removeFirstTwo(source);
+
+// ===== 16
+const stats = {
+    max: 56.78,
+    standard_deviation: 4.34,
+    median: 34.54,
+    mode: 23.87,
+    min: -0.75,
+    average: 35.85
+};
+
+// Only change code below this line
+const half = ({max, min}) => (max + min) / 2.0;
+// Only change code above this line
