5f65884949ceeb736f1e4c4e5c2aa406c5261930
D1msn <Flash.volkov@gmail.com>
2021-10-06 13:21:36 +0300

    100% complete

--- BasicDataStructures.js
+++ BasicDataStructures.js
@@ -160,3 +160,122 @@ function checkInventory(scannedItem) {
 
 console.log(checkInventory("apples"));
 
+//==== 15
+let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28,
+    bananas: 13,
+    grapes: 35,
+    strawberries: 27
+};
+
+// Only change code below this line
+delete foods.oranges;
+delete foods.plums ;
+delete foods.strawberries ;
+// Only change code above this line
+
+console.log(foods);
+
+//==== 16
+let users = {
+    Alan: {
+        age: 27,
+        online: true
+    },
+    Jeff: {
+        age: 32,
+        online: true
+    },
+    Sarah: {
+        age: 48,
+        online: true
+    },
+    Ryan: {
+        age: 19,
+        online: true
+    }
+};
+
+function isEveryoneHere(userObj) {
+    // Only change code below this line
+    if('Alan' in userObj && 'Jeff' in userObj && userObj.hasOwnProperty('Sarah') && 'Ryan' in userObj){
+        return true;
+    } return false;
+    // Only change code above this line
+}
+
+console.log(isEveryoneHere(users));
+
+//==== 17
+function countOnline(usersObj) {
+    // Only change code below this line
+    let result = 0;
+    for (let user in usersObj){
+        if(usersObj[user].online === true){
+            result++
+        }
+    }
+    return result;
+    // Only change code above this line
+}
+
+//==== 18
+let users = {
+    Alan: {
+        age: 27,
+        online: false
+    },
+    Jeff: {
+        age: 32,
+        online: true
+    },
+    Sarah: {
+        age: 48,
+        online: false
+    },
+    Ryan: {
+        age: 19,
+        online: true
+    }
+};
+
+function getArrayOfUsers(obj) {
+    // Only change code below this line
+    return Object.keys(obj)
+    // Only change code above this line
+}
+
+console.log(getArrayOfUsers(users));
+
+//==== 19
+let user = {
+    name: 'Kenneth',
+    age: 28,
+    data: {
+        username: 'kennethCodesAllDay',
+        joinDate: 'March 26, 2016',
+        organization: 'freeCodeCamp',
+        friends: [
+            'Sam',
+            'Kira',
+            'Tomo'
+        ],
+        location: {
+            city: 'San Francisco',
+            state: 'CA',
+            country: 'USA'
+        }
+    }
+};
+
+function addFriend(userObj, friend) {
+    // Only change code below this line
+    userObj.data.friends.push(friend)
+    return userObj.data.friends;
+    // Only change code above this line
+}
+
+console.log(addFriend(user, 'Pete'));
+
