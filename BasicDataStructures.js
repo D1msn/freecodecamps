0f901bc1e5f0a49915e1e2369c88d9504fefb6a7
D1msn <Flash.volkov@gmail.com>
2021-10-06 12:41:55 +0300

    70% complete

--- BasicDataStructures.js
+++ BasicDataStructures.js
@@ -85,3 +85,78 @@ function quickCheck(arr, elem) {
 
 console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
 
+//==== 11
+function filteredArray(arr, elem) {
+    let newArr = [];
+    // Only change code below this line
+    for (let i = 0; i < arr.length; i++){
+        if(arr[i].indexOf(elem) === -1){
+            newArr.push(arr[i]);
+        }
+    }
+
+    // Only change code above this line
+    return newArr;
+}
+
+console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
+
+//==== 12
+let myNestedArray = [
+    // Only change code below this line
+    ['unshift', ['loop', ['concat', "deeper", true, ['concat', false, true, 'spread', 'deepest'], 'array'], 6, 7, 1000, 'method',"deep"], 1, 2, 3, 'complex', 'nested'],
+    ['mutate', 1327.98, 'splice', 'slice', 'push'],
+    ['iterate', 1.3849, 7, '8.4876', 'arbitrary', 'depth']
+    // Only change code above this line
+];
+
+//==== 13
+let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28
+};
+
+// Only change code below this line
+foods.bananas = 13;
+foods.grapes = 35;
+foods.strawberries = 27;
+// Only change code above this line
+
+console.log(foods);
+
+//==== 13
+let userActivity = {
+    id: 23894201352,
+    date: 'January 1, 2017',
+    data: {
+        totalUsers: 51,
+        online: 42
+    }
+};
+
+// Only change code below this line
+userActivity.data.online = 45;
+// Only change code above this line
+
+console.log(userActivity);
+
+//==== 14
+let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28,
+    bananas: 13,
+    grapes: 35,
+    strawberries: 27
+};
+
+function checkInventory(scannedItem) {
+    // Only change code below this line
+    if(foods[scannedItem] != undefined){
+        return foods[scannedItem];
+    }  // Only change code above this line
+}
+
+console.log(checkInventory("apples"));
+
