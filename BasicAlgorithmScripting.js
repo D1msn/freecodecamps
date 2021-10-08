76b8d462398faba78afa760f5ebcb35ce3429cf1
D1msn <Flash.volkov@gmail.com>
2021-10-08 22:18:41 +0300

    50% complete

--- BasicAlgorithmScripting.js
+++ BasicAlgorithmScripting.js
@@ -41,3 +41,61 @@ function findLongestWordLength(str) {
 }
 
 findLongestWordLength("The quick brown fox jumped over the lazy dog");
+
+//==== 5
+function largestOfFour(arr) {
+    let results = [];
+
+    for(let i = 0; i < arr.length; i++){
+        let max = arr[i][0];
+        for(let j = 0; j < arr[i].length; j++){
+            if(arr[i][j] > max){
+                max = arr[i][j];
+            }
+        }
+        results[i] = max;
+    }
+
+
+    return results;
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+//==== 5
+function confirmEnding(str, target) {
+    return str.slice(str.length - target.length) === target;
+}
+
+confirmEnding("Bastian", "n");
+
+//==== 6
+function repeatStringNumTimes(str, num) {
+    let result = ''
+    if(num < 0){
+        return ''
+    } else{
+        let i = 0;
+        while(i < num){
+            result += str;
+            i++
+        }
+    }
+    console.log(result)
+    return result
+}
+
+repeatStringNumTimes("abc", 3);
+
+//==== 7
+function truncateString(str, num) {
+    if(str.length > num){
+        let line = str.slice(-str.length, num) + "...";
+        console.log(line)
+        return line;
+    } else{
+        return str;
+    }
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 8);
