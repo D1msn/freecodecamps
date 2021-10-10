ac26e9ee2e17f457faa46c08acd33771c56aa509
D1msn <Flash.volkov@gmail.com>
2021-10-10 22:43:01 +0300

    50% complete

--- ObjectOrientedProgramming.js
+++ ObjectOrientedProgramming.js
@@ -66,3 +66,62 @@ let myHouse  = new House(3);
 myHouse instanceof House
 
 // Only change code below this line
+
+//==== 8;
+function Bird(name) {
+    this.name = name;
+    this.numLegs = 2;
+}
+
+let canary = new Bird("Tweety");
+let ownProps = [];
+
+for(let prop in canary ){
+    if(canary.hasOwnProperty(prop)) {
+        ownProps.push(prop);
+    }
+}
+
+// Only change code below this line
+
+//==== 9;
+function Dog(name) {
+    this.name = name;
+}
+
+
+Dog.prototype.numLegs = 2;
+// Only change code above this line
+let beagle = new Dog("Snoopy");
+
+//==== 10;
+function Dog(name) {
+    this.name = name;
+}
+
+Dog.prototype.numLegs = 4;
+
+let beagle = new Dog("Snoopy");
+
+let ownProps = [];
+let prototypeProps = [];
+
+
+for (let property in beagle ) {
+    if(beagle .hasOwnProperty(property)) {
+        ownProps.push(property);
+    } else {
+        prototypeProps.push(property);
+    }
+}
+// Only change code below this line
+
+//==== 11;
+function Dog(name) {
+    this.name = name;
+}
+
+// Only change code below this line
+function joinDogFraternity(candidate) {
+    return candidate.constructor === Dog;
+}
