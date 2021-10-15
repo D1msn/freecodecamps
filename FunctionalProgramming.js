528916c30276ae3811dbe947361fb6439640792e
DmitryVolkov <volkov@cmsplanet.ru>
2021-10-15 23:42:43 +0300

    100% complete

--- FunctionalProgramming.js
+++ FunctionalProgramming.js
@@ -418,3 +418,68 @@ function alphabeticalOrder(arr) {
     // Only change code above this line
 }
 alphabeticalOrder(["a", "d", "c", "a", "z", "g"]);
+
+// 18
+
+var globalArray = [5, 6, 3, 2, 9];
+function nonMutatingSort(arr) {
+    // Only change code below this line
+    return arr.slice().sort((a,b) => a - b);
+    // Only change code above this line
+}
+nonMutatingSort(globalArray);
+
+// 19
+
+function splitify(str) {
+    // Only change code below this line
+    return str.split(/\W/);
+    // Only change code above this line
+}
+splitify("Hello World,I-am code");
+
+// 20
+
+function sentensify(str) {
+    // Only change code below this line
+    return str.split(/\W/).join(' ');
+
+    // Only change code above this line
+}
+sentensify("May-the-force-be-with-you");
+
+// 21
+
+// Only change code below this line
+function urlSlug(title) {
+    return title.toLowerCase().split(' ').filter(item => item != "").join('-');
+}
+// Only change code above this line
+
+// 22
+
+function checkPositive(arr) {
+    // Only change code below this line
+    return arr.every((value) => value > 0);
+
+    // Only change code above this line
+}
+checkPositive([1, 2, 3, -4, 5]);
+
+// 23
+
+function checkPositive(arr) {
+    // Only change code below this line
+    return arr.some(a => a>0)
+    // Only change code above this line
+}
+checkPositive([1, 2, 3, -4, 5]);
+
+// 24
+
+function add(x) {
+    // Only change code below this line
+    return y => z => x + y + z;
+    // Only change code above this line
+}
+add(10)(20)(30);
