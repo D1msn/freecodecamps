1271b25c002bc0b5cf7e86e3509657aed9094573
D1msn <flash.volkov@gmail.com>
2021-10-16 23:39:58 +0300

    30% complete

--- RegularExpressions.js
+++ RegularExpressions.js
@@ -24,3 +24,24 @@ let extractStr = "Extract the word 'coding' from this string.";
 let codingRegex = /coding/; // Change this line
 
 let result = extractStr.match(codingRegex); // Change this line
+
+
+// ==== 6;
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /Twinkle/gi; // Change this line
+let result = twinkleStar.match(starRegex); // Change this line
+
+// ==== 7;
+let exampleStr = "Let's have fun with regular expressions!";
+let unRegex = /.un/; // Change this line
+let result = unRegex.test(exampleStr);
+
+// ==== 8;
+let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+let vowelRegex = /[aeiou]/gi; // Change this line
+let result = quoteSample.match(vowelRegex); // Change this line
+
+// ==== 9;
+let quoteSample = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/gi; // Change this line
+let result = quoteSample.match(alphabetRegex); // Change this line
